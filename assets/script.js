acb4dc064d90c2acfaa5cbb4e306e210f60d3fcc
WD40 <69653174+duncangw1@users.noreply.github.com>
2020-10-01 18:30:22 -0400

    Started creating colorChange function for the event text areas.

--- assets/script.js
+++ assets/script.js
@@ -7,4 +7,19 @@ $(document).ready(function () {
 
   // Calling writeDate function
   writeDate();
+
+  colorChange();
+
+  function colorChange() {
+    var currentHour = moment().format("HH");
+    console.log(currentHour);
+
+    if (currentHour < 9 || currentHour > 17) {
+      if (currentHour < 9) {
+        $(".eventTextArea").addClass("future");
+      } else if (currentHour > 17) {
+        $(".eventTextArea").addClass("past");
+      }
+    }
+  }
 });
